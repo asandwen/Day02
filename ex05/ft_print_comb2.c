4867deca468191ed92f6cd9b99a160c9c08a0d6a
asandwen <66949179+asandwen@users.noreply.github.com>
2020-06-23 13:15:18 +0200

    git

---
+++ ex05/ft_print_comb2.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asandwen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/21 11:57:28 by asandwen          #+#    #+#             */
+/*   Updated: 2020/06/23 13:14:31 by asandwen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+int ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return 0;
+}
+void ft_print_comb2(void)
+{
+	int x;
+	int y;
+	x = 0;
+	while (x < 99)
+	{
+		y = x + 1;
+		while(y <= 99)
+		{
+			ft_putchar(x/10 + '0');
+			ft_putchar(x%10 + '0');
+			ft_putchar(' ');
+			ft_putchar(y/10 + '0');
+			ft_putchar(y%10 + '0');
+			ft_putchar(',');
+			ft_putchar(' ');
+			y++;
+
+		}
+		x++;
+	}
+}
